f24730b9d5afd4589c22a607a70c2fc8e0aaf998
vXofi <79092187+vXofi@users.noreply.github.com>
2023-12-15 17:05:23 +0300

    Delete labs/src/ru/mirea/lab22/Task2Chairs/MultifunctionalChairFactory.java

--- labs/src/ru/mirea/lab22/Task2Chairs/MultifunctionalChairFactory.java
+++
@@ -1,8 +0,0 @@
-package ru.mirea.lab22.Task2Chairs;
-
-class MultifunctionalChairFactory extends ChairFactory {
-    @Override
-    Chair createChair() {
-        return new MultifunctionalChair();
-    }
-}
